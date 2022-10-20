0d6288a512c0189ae09058e5069a4f0984f78f25
Ivaylo Rachev <rachev.ivaylo@yahoo.fr>
2022-10-20 09:57:22 +0200

    Modification factory
    Modification du factory pattern pour ajouter les infos des photographes

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,44 @@
+//function to show the informations on index page
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { id, name, portrait, tagline, city, country, price } = data
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/${portrait}`
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement('article');
+        const link = document.createElement('a')
+
+        link.href = './photographer.html?id=' + id
+        link.ariaLabel = name
+
+        const img = document.createElement('img')
+
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        img.alt = 'Photo du profil de ' + name
+
+        const h2 = document.createElement('h2')
+
+        h2.textContent = name
+
+        article.appendChild(link)
+        link.appendChild(img)
+        link.appendChild(h2)
+
+        const div = document.createElement('div')
+        const locationInfos = document.createElement('p')
+        const tagLineInfos = document.createElement('p')
+        const priceInfos = document.createElement('p')
+
+        locationInfos.textContent = city + ', ' + country
+        tagLineInfos.textContent = tagline
+        priceInfos.textContent = price + '€/jour'
+
+        div.appendChild(locationInfos)
+        div.appendChild(tagLineInfos)
+        div.appendChild(priceInfos)
+        article.appendChild(div)
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { getUserCardDOM }
 }
