112d0e008a950fb0f2217c39788824f1da1a15b8
Ivaylo Rachev <rachev.ivaylo@yahoo.fr>
2022-10-25 10:48:59 +0200

    Update photographer.js
    Ajout de la fonction pour afficher les infos des photographes

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -14,3 +14,23 @@ async function getPhotographers() {
     }
 }
 //fin de get the datas from json
+
+//fotographer informations
+function fillheader(photographer) {
+    const { name, city, country, tagline, portrait } = photographer
+    const nameElement = document.querySelector('.photograph_infos > h1')
+    const locationElement = document.querySelector('.photograph_infos > p:ntn-child(2)')
+    const taglineElement = document.querySelector('.photograph_infos > p:last-child')
+
+    nameElement.textContent = name
+    locationElement.textContent = city + ', ' + country
+    taglineElement.textContent = tagline
+
+    const header = document.querySelector('.photograph-header')
+    const image = document.createElement('img')
+
+    image.src = `./assets/photographers/${portrait}`
+    image.alt = photographer.name
+
+    header.appendChild(image)
+}
