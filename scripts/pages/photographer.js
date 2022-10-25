e47c807346172d19681799a3e30c90d22df3d194
Ivaylo Rachev <rachev.ivaylo@yahoo.fr>
2022-10-25 10:28:36 +0200

    Update photographer.js
    Ajout de l'API fetch

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,14 @@
-//Mettre le code JavaScript lié à la page photographer.html
+async function getPhotographers() {
+    try {
+        const response = await fetch('./data/photographers.json')
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.error(error)
+        const errorElement = document.createElement('h2')
+        errorElement.classList.add('photographers_error')
+        errorElement.textContent = 'Une erreur est survenue.'
+        main.appendChild(errorElement)
+        return { photographers: [] }
+    }
+}
