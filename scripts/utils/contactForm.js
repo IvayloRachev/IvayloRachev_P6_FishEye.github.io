7a2be154c0b3107cbcd68d49db8dff38c87466b5
Ivaylo Rachev <rachev.ivaylo@yahoo.fr>
2022-10-26 12:09:58 +0200

    Update contactForm.js
    Ajout des commentaires

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,13 +1,18 @@
+//function open modal
 function displayModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "block";
 }
+//fin de function open modal
 
+//function close modal
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
+//fin de fonction close modal
 
+//function send message and display infos in console.log
 function sendMessage(event) {
     event.preventDefault()
     for (const element of event.target.elements) {
@@ -17,3 +22,4 @@ function sendMessage(event) {
         }
     }
 }
+//fin de function send message and display infos in console.log
