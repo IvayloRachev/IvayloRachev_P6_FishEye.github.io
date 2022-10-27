7d6c6584303da7fe7ba71983dae07fcf8b5f2ae7
Ivaylo Rachev <rachev.ivaylo@yahoo.fr>
2022-10-27 17:59:55 +0200

    create media factory
    Ajout de média factory

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -159,3 +159,17 @@ function videos(data) {
     }
 }
 //fin de vidéos factory
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//media factory
+function mediaFactory(data, type) {
+    if (type == "image") {
+        return new images(data)
+    } else if (type == "video") {
+        return new videos(data)
+    } else {
+        throw "Unknown type"
+    }
+}
+//fin de media factory
