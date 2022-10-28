1f85f7d31543cba2d60af90d607bd42ed6b88d47
Ivaylo Rachev <rachev.ivaylo@yahoo.fr>
2022-10-28 11:29:38 +0200

    Update images.js
    Modification de l'images factory

--- scripts/factories/images.js
+++ scripts/factories/images.js
@@ -1,50 +1,59 @@
 //images factory
-function images(data) {
-    const { title, likes, image, price, date, liked = false } = data
+class Images {
+    constructor(data){
+        this.title = data.title;
+        this.likes = data.likes;
+        this.image = data.image;
+        this.price = data.price;
+        this.date = data.date;
+        this.liked = false;
+    }
 
-    function getImageCardDOM() {
-        const divImgContent = document.createElement('div')
+    getImageCardDOM() {
+        const divImgContent = document.createElement('div');
 
-        const picture = `./assets/images/${image}`
-        const img = document.createElement('img')
-        img.setAttribute('src', picture)
-        img.className = "media_photographe"
-        img.setAttribute('alt', title)
-        linkPicture.appendChild(img)
-        divImgContent.appendChild(linkPicture)
+        const picture = `./assets/images/${this.image}`;
+        const linkPicture = document.createElement('a');
 
-        const titleMedia = document.createElement('div')
-        const h3 = document.createElement('h3')
-        h3.className = "titreMedia"
-        h3.textContent = title
-        titleMedia.appendChild(h3)
-        divImgContent.appendChild(titleMedia)
+        const img = document.createElement('img');
+        img.setAttribute('src', picture);
+        img.className = "media_photographe";
+        img.setAttribute('alt', this.title);
+        linkPicture.appendChild(img);
+        divImgContent.appendChild(linkPicture);
 
-        const likeMedia = document.createElement('div')
-        likeMedia.id = 'mediaLike'
-        let totalLike = document.createElement('p')
-        const iconHeart = document.createElement('i')
-        iconHeart.className = "fas fa-heart"
-        iconHeart.setAttribute('title', "nombre de likes")
-        totalLike.textContent = likes
-        totalLike.className = "total_like"
-        iconHeart.addEventListener("click", function(e) {
-            e.preventDefault()
-            let increment_total_like = document.getElementById('all_likes')
-            if (liked) {
-                parseInt(totalLike.innerHTML--)
-                parseInt(increment_total_like.innerHTML--)
-            } else {
-                parseInt(totalLike.innerHTML++)
-                parseInt(increment_total_like.innerHTML++)
+        const titleMedia = document.createElement('div');
+        const h3 = document.createElement('h3');
+        h3.className = "titreMedia";
+        h3.textContent = this.title;
+        titleMedia.appendChild(h3);
+        divImgContent.appendChild(titleMedia);
+
+        const likeMedia = document.createElement('div');
+        likeMedia.id = "mediaLike";
+        let totalLike = document.createElement('p');
+        const iconHeart = document.createElement('i');
+        iconHeart.className = "fas fa-heart";
+        iconHeart.setAttribute("title", "nombre de likes");
+        totalLike.textContent = this.likes;
+        totalLike.className = "total_like";
+        iconHeart.addEventListener("click", function (e) {
+            e.preventDefault();
+            let increment_total_like = document.getElementById("all_likes");
+            if (this.liked) {
+                parseInt(totalLike.innerHTML--);
+                parseInt(increment_total_like.innerHTML--);
+            }else {
+                parseInt(totalLike.innerHTML++);
+                parseInt(increment_total_like.innerHTML++);
             }
-            liked = !liked
-        })
-        likeMedia.appendChild(totalLike)
-        likeMedia.appendChild(iconHeart)
+            this.liked = !this.liked;
+        });
+        likeMedia.appendChild(totalLike);
+        likeMedia.appendChild(iconHeart);
 
-        divImgContent.appendChild(likeMedia)
-        return divImgContent
+        divImgContent.appendChild(likeMedia);
+        return divImgContent;
     }
 }
 //fin de images factory
