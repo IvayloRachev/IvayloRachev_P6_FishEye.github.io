a81313796e49f32e7568cea928d39b11df55cad5
Ivaylo Rachev <rachev.ivaylo@yahoo.fr>
2022-11-01 18:01:18 +0100

    Function to display photographer informations photgrapher.js
    Ajout de la fonction pour afficher les infos des photographes

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -5,13 +5,16 @@ let photographer
 let medias
 const likes = []
 
+
 //API fetch
 ;(async () => {
     try {
-        const response = await fetch('./data.photographers.json')
-        const data = await response.json()
+        const response = await fetch('./data/photographers.json')
+		const data = await response.json()
 
         photographer = data.photographers.find((photographer) => photographer.id === photographerId)
+
+        photographerHeader(photographer)
     }catch (error) {
         console.error(error)
         const errorElement = document.createElement('h2')
@@ -21,3 +24,23 @@ const likes = []
     }
 })()
 //fin de API fetch
+
+//photographer informations
+function photographerHeader(photographer) {
+	const { name, city, country, tagline, portrait } = photographer
+	const nameElement = document.querySelector('.photograph_infos > h1')
+	const locationElement = document.querySelector('.photograph_infos > p:nth-child(2)')
+	const taglineElement = document.querySelector('.photograph_infos > p:last-child')
+
+	nameElement.textContent = name
+	locationElement.textContent = city + ', ' + country
+	taglineElement.textContent = tagline
+
+	const header = document.querySelector('.photograph-header')
+	const image = document.createElement('img')
+
+	image.src = `./assets/photographers/${portrait}`
+	image.alt = photographer.name
+
+	header.appendChild(image)
+}
