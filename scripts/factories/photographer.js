d57d9b0c520ebea15f3167c0eac63006ab22ab52
Ivaylo Rachev <rachev.ivaylo@yahoo.fr>
2022-11-14 14:46:18 +0100

    Update photographerFactory
    Résolution des problèmes ESlint

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -13,7 +13,7 @@ function photographerFactory(data) {
                 <img class="profil" src="${picture}" alt="photo du profil de ${name}">
                 <h2 class="name">${name}</h2>
             </a>
-            <div>
+            <div role="presentation" aria-label="informations photographes" tabindex="0">
                 <p>${city}, ${country}</p>
                 <p>${tagline}</p>
                 <p>${price} €/jour</p>
@@ -23,4 +23,5 @@ function photographerFactory(data) {
     }
     return { getUserCardDOM }
 }
+photographerFactory
 //fin de function to create and display the photographers informations on index page
